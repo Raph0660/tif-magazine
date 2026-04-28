2c159ae3ccd1340b62d1f91b980637063f5afd31
Raph0660 <raphael.geronimi1@gmail.com>
2026-04-28 15:53:05 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+import './globals.css'
 
+export const metadata = {
+  title: 'Tif et ses Gourmandises',
+  description: 'Le magazine culinaire de luxe',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
