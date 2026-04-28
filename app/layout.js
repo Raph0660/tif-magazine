c229d2ef6ddc070a21232a6ad7abd75f13d63921
Raph0660 <raphael.geronimi1@gmail.com>
2026-04-28 15:59:17 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,9 @@
 import './globals.css'
 
-export const metadata = {
-  title: 'Tif et ses Gourmandises',
-  description: 'Le magazine culinaire de luxe',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body className="bg-stone-50">{children}</body>
     </html>
   )
 }
